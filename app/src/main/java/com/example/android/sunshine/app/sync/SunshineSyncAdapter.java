6b3c791b3999b37ab4a5764ac6ac7abb059aa772
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-03-24 17:58:05 +0530

    Add google client api to sync adapter, Add singleton PreferenceManager

--- app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -12,7 +12,6 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.SyncRequest;
 import android.content.SyncResult;
 import android.content.res.Resources;
@@ -22,8 +21,8 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.text.format.Time;
@@ -36,6 +35,15 @@ import com.example.android.sunshine.app.R;
 import com.example.android.sunshine.app.Utility;
 import com.example.android.sunshine.app.data.WeatherContract;
 import com.example.android.sunshine.app.muzei.WeatherMuzeiSource;
+import com.example.android.sunshine.app.utils.Debug;
+import com.example.android.sunshine.app.utils.PreferenceManager;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -59,12 +67,12 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     // Interval at which to sync with the weather, in seconds.
     // 60 seconds (1 minute) * 180 = 3 hours
     public static final int SYNC_INTERVAL = 60 * 180;
-    public static final int SYNC_FLEXTIME = SYNC_INTERVAL/3;
+    public static final int SYNC_FLEXTIME = SYNC_INTERVAL / 3;
     private static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
     private static final int WEATHER_NOTIFICATION_ID = 3004;
 
 
-    private static final String[] NOTIFY_WEATHER_PROJECTION = new String[] {
+    private static final String[] NOTIFY_WEATHER_PROJECTION = new String[]{
             WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
             WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
             WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
@@ -77,9 +85,20 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     private static final int INDEX_MIN_TEMP = 2;
     private static final int INDEX_SHORT_DESC = 3;
 
+    private static final String INFO_FOR_WEAR = "/weather-info-for-wear";
+    private static final String INFO_HIGH = "high";
+    private static final String INFO_LOW = "low";
+    private static final String INFO_WEATHER = "weatherId";
+    private static final String INFO_KEY = "key";
+
+    private static final String LAST_LOW_REPORTED = "last_low_reported";
+    private static final String LAST_HIGH_REPORTED = "last_high_reported";
+    private static final String LAST_WEATHER_REPORTED = "last_weather_reported";
+
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({LOCATION_STATUS_OK, LOCATION_STATUS_SERVER_DOWN, LOCATION_STATUS_SERVER_INVALID,  LOCATION_STATUS_UNKNOWN, LOCATION_STATUS_INVALID})
-    public @interface LocationStatus {}
+    @IntDef({LOCATION_STATUS_OK, LOCATION_STATUS_SERVER_DOWN, LOCATION_STATUS_SERVER_INVALID, LOCATION_STATUS_UNKNOWN, LOCATION_STATUS_INVALID})
+    public @interface LocationStatus {
+    }
 
     public static final int LOCATION_STATUS_OK = 0;
     public static final int LOCATION_STATUS_SERVER_DOWN = 1;
@@ -87,8 +106,65 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     public static final int LOCATION_STATUS_UNKNOWN = 3;
     public static final int LOCATION_STATUS_INVALID = 4;
 
+    // setup google client api for communication with wear device
+    private GoogleApiClient mGoogleApiClient;
+
     public SunshineSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
+    }
+
+    private void initGoogleApiClient() {
+        if (null == mGoogleApiClient) {
+            Debug.c();
+            mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                    .addApi(Wearable.API)
+                    .build();
+            // not setting callbacks
+            // if we want to set callbacks, then we can do like this
+            // set up a local broadcast when weather info is fetched
+            // then in respective broadcast listener, init google client
+            // and also have callbacks
+        }
+    }
+
+    private void postDataForWearDevice(final double lowTemp, final double highTemp, final int weatherId) {
+        initGoogleApiClient();
+        if (null != mGoogleApiClient) {
+            Debug.c();
+            mGoogleApiClient.connect();
+            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(INFO_FOR_WEAR);
+            DataMap map = putDataMapRequest.getDataMap();
+            map.putString(INFO_HIGH, Utility.formatTemperature(getContext(), highTemp));
+            map.putString(INFO_LOW, Utility.formatTemperature(getContext(), lowTemp));
+            map.putInt(INFO_WEATHER, weatherId);
+
+            // to make this data overwrite previous one,
+            // there has to be a change, hence using current time
+            long time = System.currentTimeMillis();
+            map.putString(INFO_KEY, Long.toString(time));
+
+            PutDataRequest request = putDataMapRequest.asPutDataRequest();
+            Wearable.DataApi.putDataItem(mGoogleApiClient, request)
+                    .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                        @Override
+                        public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
+                            if (!dataItemResult.getStatus().isSuccess()) {
+                                Debug.e("Error: parcel company didnot pickup parcel", false);
+                            } else {
+                                Debug.c();
+                                PreferenceManager.getInstance(getContext()).writeValue
+                                        (LAST_LOW_REPORTED, lowTemp);
+                                PreferenceManager.getInstance(getContext()).writeValue
+                                        (LAST_HIGH_REPORTED, highTemp);
+                                PreferenceManager.getInstance(getContext()).writeValue
+                                        (LAST_WEATHER_REPORTED, weatherId);
+                                PreferenceManager.getInstance(getContext()).debug();
+                            }
+                        }
+                    });
+        } else {
+            Debug.e("mGoogleApiClient should not be null", false);
+        }
     }
 
     @Override
@@ -186,7 +262,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     /**
      * Take the String representing the complete forecast in JSON Format and
      * pull out the data we need to construct the Strings needed for the wireframes.
-     *
+     * <p/>
      * Fortunately parsing is easy:  constructor takes the JSON string and converts it
      * into an Object hierarchy for us.
      */
@@ -232,7 +308,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
             JSONObject forecastJson = new JSONObject(forecastJsonStr);
 
             // do we have an error?
-            if ( forecastJson.has(OWM_MESSAGE_CODE) ) {
+            if (forecastJson.has(OWM_MESSAGE_CODE)) {
                 int errorCode = forecastJson.getInt(OWM_MESSAGE_CODE);
 
                 switch (errorCode) {
@@ -278,7 +354,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
             // now we work exclusively in UTC
             dayTime = new Time();
 
-            for(int i = 0; i < weatherArray.length(); i++) {
+            for (int i = 0; i < weatherArray.length(); i++) {
                 // These are the values that will be collected.
                 long dateTime;
                 double pressure;
@@ -296,7 +372,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 JSONObject dayForecast = weatherArray.getJSONObject(i);
 
                 // Cheating to convert this to UTC time, which is what we want anyhow
-                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                dateTime = dayTime.setJulianDay(julianStartDay + i);
 
                 pressure = dayForecast.getDouble(OWM_PRESSURE);
                 humidity = dayForecast.getInt(OWM_HUMIDITY);
@@ -316,6 +392,22 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 high = temperatureObject.getDouble(OWM_MAX);
                 low = temperatureObject.getDouble(OWM_MIN);
 
+                if (0 == i) {
+                    PreferenceManager.getInstance(getContext()).debug();
+                    // check if we have reported this weather condition
+                    double lastLowReported = PreferenceManager.getInstance(getContext()).readValue
+                            (LAST_LOW_REPORTED, 0.0d);
+                    double lastHighReported = PreferenceManager.getInstance(getContext()).readValue
+                            (LAST_HIGH_REPORTED, 0.0d);
+                    double lastWeatherReported = PreferenceManager.getInstance(getContext()).readValue
+                            (LAST_WEATHER_REPORTED, -1);
+                    if (Double.compare(lastLowReported, low) != 0
+                            || Double.compare(lastHighReported, high) != 0
+                            || lastWeatherReported != weatherId) {
+                        postDataForWearDevice(low, high, weatherId);
+                    }
+                }
+
                 ContentValues weatherValues = new ContentValues();
 
                 weatherValues.put(WeatherContract.WeatherEntry.COLUMN_LOC_KEY, locationId);
@@ -334,7 +426,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
             int inserted = 0;
             // add to database
-            if ( cVVector.size() > 0 ) {
+            if (cVVector.size() > 0) {
                 ContentValues[] cvArray = new ContentValues[cVVector.size()];
                 cVVector.toArray(cvArray);
                 getContext().getContentResolver().bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, cvArray);
@@ -342,7 +434,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 // delete old data so we don't build up an endless history
                 getContext().getContentResolver().delete(WeatherContract.WeatherEntry.CONTENT_URI,
                         WeatherContract.WeatherEntry.COLUMN_DATE + " <= ?",
-                        new String[] {Long.toString(dayTime.setJulianDay(julianStartDay-1))});
+                        new String[]{Long.toString(dayTime.setJulianDay(julianStartDay - 1))});
 
                 updateWidgets();
                 updateMuzei();
@@ -379,15 +471,17 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     private void notifyWeather() {
         Context context = getContext();
         //checking the last update and notify if it' the first of the day
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        PreferenceManager.getInstance(getContext()).debug();
         String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
-        boolean displayNotifications = prefs.getBoolean(displayNotificationsKey,
+        boolean displayNotifications = PreferenceManager.getInstance(context).readValue(displayNotificationsKey,
                 Boolean.parseBoolean(context.getString(R.string.pref_enable_notifications_default)));
 
-        if ( displayNotifications ) {
+        if (displayNotifications) {
 
             String lastNotificationKey = context.getString(R.string.pref_last_notification);
-            long lastSync = prefs.getLong(lastNotificationKey, 0);
+            long defaultValue = 0L;
+            long lastSync = PreferenceManager.getInstance(context).readValue(lastNotificationKey,
+                    defaultValue);
 
             if (System.currentTimeMillis() - lastSync >= DAY_IN_MILLIS) {
                 // Last sync was more than 1 day ago, let's send a notification with the weather.
@@ -474,9 +568,9 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                     mNotificationManager.notify(WEATHER_NOTIFICATION_ID, mBuilder.build());
 
                     //refreshing last sync
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putLong(lastNotificationKey, System.currentTimeMillis());
-                    editor.commit();
+                    long currTime = System.currentTimeMillis();
+                    PreferenceManager.getInstance(context).writeValue(lastNotificationKey,
+                            currTime);
                 }
                 cursor.close();
             }
@@ -487,9 +581,9 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
      * Helper method to handle insertion of a new location in the weather database.
      *
      * @param locationSetting The location string used to request updates from the server.
-     * @param cityName A human-readable city name, e.g "Mountain View"
-     * @param lat the latitude of the city
-     * @param lon the longitude of the city
+     * @param cityName        A human-readable city name, e.g "Mountain View"
+     * @param lat             the latitude of the city
+     * @param lon             the longitude of the city
      * @return the row ID of the added location.
      */
     long addLocation(String locationSetting, String cityName, double lat, double lon) {
@@ -554,6 +648,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
     /**
      * Helper method to have the sync adapter sync immediately
+     *
      * @param context The context used to access the account service
      */
     public static void syncImmediately(Context context) {
@@ -582,7 +677,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 context.getString(R.string.app_name), context.getString(R.string.sync_account_type));
 
         // If the password doesn't exist, the account doesn't exist
-        if ( null == accountManager.getPassword(newAccount) ) {
+        if (null == accountManager.getPassword(newAccount)) {
 
         /*
          * Add the account and account type, no password or user data
@@ -627,13 +722,11 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     /**
      * Sets the location status into shared preference.  This function should not be called from
      * the UI thread because it uses commit to write to the shared preferences.
-     * @param c Context to get the PreferenceManager from.
+     *
+     * @param c              Context to get the PreferenceManager from.
      * @param locationStatus The IntDef value to set
      */
-    static private void setLocationStatus(Context c, @LocationStatus int locationStatus){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
-        SharedPreferences.Editor spe = sp.edit();
-        spe.putInt(c.getString(R.string.pref_location_status_key), locationStatus);
-        spe.commit();
+    static private void setLocationStatus(Context c, @LocationStatus int locationStatus) {
+        PreferenceManager.getInstance(c).writeValue(c.getString(R.string.pref_location_status_key), locationStatus);
     }
 }
---
+++ app/src/main/java/com/example/android/sunshine/app/utils/PreferenceManager.java
@@ -0,0 +1,80 @@
+package com.example.android.sunshine.app.utils;
+
+import android.content.SharedPreferences;
+import android.content.Context;
+
+
+/**
+ * Created by Amrendra Kumar on 23/11/15.
+ */
+public class PreferenceManager {
+    private static final String SHARED_PREF_FILE_NAME = "sunshine-weather-info";
+
+    SharedPreferences mSharedPreference;
+    SharedPreferences.Editor mEditor;
+
+    static PreferenceManager mSharedPreferenceMgr = null;
+
+    private PreferenceManager(Context context) {
+        mSharedPreference = android.preference.PreferenceManager.getDefaultSharedPreferences
+                (context);
+        // context.getSharedPreferences(SHARED_PREF_FILE_NAME, Context.MODE_PRIVATE);
+        mEditor = mSharedPreference.edit();
+    }
+
+    public static PreferenceManager getInstance(Context context) {
+        if (mSharedPreferenceMgr == null) {
+            mSharedPreferenceMgr = new PreferenceManager(context);
+        }
+        return mSharedPreferenceMgr;
+    }
+
+    public int readValue(final String key, final int defaultValue) {
+        return mSharedPreference.getInt(key, defaultValue);
+    }
+
+    public void writeValue(final String key, final int value) {
+        mEditor.putInt(key, value).commit();
+    }
+
+    public long readValue(final String key, final long defaultValue) {
+        return mSharedPreference.getLong(key, defaultValue);
+    }
+
+    public void writeValue(final String key, final long value) {
+        mEditor.putLong(key, value).commit();
+    }
+
+    public double readValue(final String key, final double defaultValue) {
+        return Double.longBitsToDouble(mSharedPreference.getLong(key, Double.doubleToLongBits(defaultValue)));
+    }
+
+    public void writeValue(final String key, final double value) {
+        mEditor.putLong(key, Double.doubleToRawLongBits(value)).commit();
+    }
+
+    public boolean readValue(String key, boolean defaultValue) {
+        return mSharedPreference.getBoolean(key, defaultValue);
+    }
+
+    public void writeValue(final String key, final boolean value) {
+        mEditor.putBoolean(key, value).commit();
+    }
+
+    public String readValue(final String key, final String defaultValue) {
+        return mSharedPreference.getString(key, defaultValue);
+    }
+
+    public void writeValue(final String key, final String value) {
+        mEditor.putString(key, value).commit();
+    }
+
+    public void clear() {
+        mEditor.clear().commit();
+    }
+
+
+    public void debug() {
+        Debug.preferences(mSharedPreference);
+    }
+}
